3fe3d419520669caf3f8a5e2bc362461f41e29a3
luatnt <luatnt@smartosc.com>
2020-10-01 18:21:08 +0700

    HCMFRE-644 add module feature product

---
+++ app/code/Team2FeaturedProducts/FeaturedProducts/Block/Widget/FeaturedProducts.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Team2FeaturedProducts\FeaturedProducts\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class FeaturedProducts extends Template implements BlockInterface
+{
+    protected $_template = 'widget/FeaturedProducts.phtml';
+}
