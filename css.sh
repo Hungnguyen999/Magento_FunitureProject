30d89646ce87c6ead5609049c5faa908e58b9166
Hunglapro999 <hungnd2@smartosc.com>
2020-11-26 17:25:15 +0700

    hung fix

--- css.sh
+++ css.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo chmod -R 777 var/ pub/ generated/;
-rm -rf /pub/static/*;
-rm -rf /var/view_preprocessed/*;
+rm -rf /pub/static/*;rm -rf /var/view_preprocessed/*; rm -rf var/cache/*; rm -rf generated/*;
 bin/magento c:f;
+bin/magento setup:static-content:deploy --theme Team2/LumaTheme -f;
 sudo chmod -R 777 var/ pub/ generated/
